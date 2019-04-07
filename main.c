3dbf23421d8e45644fbf8085b7ea01d4ba4cd612
s21600065 <you_maybe@naver.com>
2019-04-07 15:30:45 +0900

    파일 없는 경우 에러메시지 표시 후, 새로 만들지 묻기 구현

--- main.c
+++ main.c
@@ -5,9 +5,19 @@ int main(int argc, char* argv[]) {
   LOGIN* userlist[100]; // 사용자목록 포인터 배열 (최대 100)
   int is_login=0; // 로그인 여부 (0 NO, 1 Yes)
   int menu_id;
+  int new_file;
+  FILE * fp;
   if (argc != 2) {
     printf("Usage : manager <datafile>\n");
     return 0;
+  }
+  else {
+    printf ("%s file not exist! make anyway? (Yes 1, No 2) >> ", argv[1]);
+    scanf ("%d", &new_file);
+    if (new_file == 1)
+	fp = fopen (argv[1], "w");
+    else
+	return 0;
   }
   int count = load_file(userlist, argv[1]);
   while(1){
@@ -19,5 +29,6 @@ int main(int argc, char* argv[]) {
     else break;
   }
   save_file(userlist, count, argv[1]);
+  fclose(fp);
   return 0;
 }
